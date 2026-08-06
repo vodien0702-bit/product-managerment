baab88baae96ef17ccc00433f7958271403fafd0
Võ Hoàng Diện <vodien0702@gmail.com>
2026-08-06 21:41:32 +0700

    edit file database

--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
+let isConnected = false;
+
 module.exports.connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("CONNECT SUCCESS!")
-    } catch (error) {
-        console.log("Connect Error:", error);
-    }
-}
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URL);
+    isConnected = db.connections[0].readyState;
+    console.log("Connect Success!");
+  } catch (error) {
+    console.log("Connect Error:", error);
+  }
+};
