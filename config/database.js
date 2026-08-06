5d7e7f33cd484b83bb7f6363fb940e4eb2831c4b
Võ Hoàng Diện <vodien0702@gmail.com>
2026-08-06 20:56:44 +0700

    sửa url để connect với mongo offline

--- config/database.js
+++ config/database.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 
-let isConnected = false; // Biến lưu trạng thái kết nối
-
 module.exports.connect = async () => {
-  if (isConnected) {
-    return;
-  }
-
-  try {
-    const db = await mongoose.connect(process.env.MONGO_URL);
-    isConnected = db.connections[0].readyState;
-    console.log("CONNECT SUCCESS!");
-  } catch (error) {
-    console.log("Connect Error:", error);
-  }
-};
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("CONNECT SUCCESS!")
+    } catch (error) {
+        console.log("Connect Error:", error);
+    }
+}
--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const methodOverride = require("method-override");
 const bodyParser = require('body-parser');
 const flash = require('express-flash');
 const cookieParser = require('cookie-parser')
-const session = require('express-session');
-const path = require('path');
+const session = require('express-session')
 
 require("dotenv").config();
 const route = require("./routes/client/index.route.js");
@@ -33,17 +32,16 @@ app.use(flash());
 // body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 // end body-parser
-app.set("views", path.join(__dirname, "views"));
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
 routeAdmin(app);
 route(app);
 
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(`${__dirname}/public`));
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-module.exports = app
