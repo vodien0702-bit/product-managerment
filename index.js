e344ae2488c144e268f5ae4fb141c78f79490866
Võ Hoàng Diện <vodien0702@gmail.com>
2026-08-06 21:21:26 +0700

    sửa file index

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const methodOverride = require("method-override");
 const bodyParser = require('body-parser');
 const flash = require('express-flash');
 const cookieParser = require('cookie-parser')
-const session = require('express-session')
+const session = require('express-session');
+const path = require('path');
 
 require("dotenv").config();
 const route = require("./routes/client/index.route.js");
@@ -32,14 +33,14 @@ app.use(flash());
 // body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 // end body-parser
-app.set("views", `${__dirname}/views`);
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 routeAdmin(app);
 route(app);
 
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 app.listen(port, () => {
